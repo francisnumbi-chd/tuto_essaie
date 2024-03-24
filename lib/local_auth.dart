7a7e19b14e537c9b5145a464d7fe7be0bd82c00a
Francis Nduba Numbi <francisnnumbi@yandex.com>
2024-03-24 22:58:48 +0200

    testing

--- lib/local_auth.dart
+++ lib/local_auth.dart
@@ -5,8 +5,29 @@ class LocalAuth {
 
   static final LocalAuthentication _auth = LocalAuthentication();
 
+  static Future<bool> get _canAuthenticateBiom async {
+    if (!await _auth.canCheckBiometrics) return false;
+    List<BiometricType> availableBiometrics =
+        await _auth.getAvailableBiometrics();
+    print(availableBiometrics);
+    if (availableBiometrics.isEmpty) return false;
+
+    /*   if (availableBiometrics.contains(BiometricType.face) ||
+        availableBiometrics.contains(BiometricType.fingerprint)) {
+      return true;
+    }*/
+
+    if (availableBiometrics.contains(BiometricType.face) ||
+        availableBiometrics.contains(BiometricType.fingerprint) ||
+        availableBiometrics.contains(BiometricType.iris)) {
+      return true;
+    }
+
+    return false;
+  }
+
   static Future<bool> _canAuthenticate() async =>
-      await _auth.canCheckBiometrics || await _auth.isDeviceSupported();
+      await _canAuthenticateBiom || await _auth.isDeviceSupported();
 
   static Future<bool> authenticate() async {
     try {
@@ -14,7 +35,7 @@ class LocalAuth {
       return await _auth.authenticate(
         localizedReason: 'Authenticate to access the app',
         options: const AuthenticationOptions(
-          //  sensitiveTransaction: true,
+          sensitiveTransaction: true,
           useErrorDialogs: true,
           stickyAuth: true,
           biometricOnly: true,
